a902948ffc525464e0686b9ad280d7b87937933b
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2019-10-14 12:41:07 -0600

    finished the comments and other minor details

--- README.md
+++ README.md
@@ -6,6 +6,17 @@
 * [Installing the Text Tools](doc/Installing_Text_Tools.md)
 * [WorkLog](doc/WorkLog.txt)
 
+our last project was a great success! The customer was very pleased with their
+new Text Tools and so impressed with the quality of my work that they have
+decided to contract me to finish the entire project for them.
+my task is to analyze a large body of data and produce a report of the
+findings.  This program summarizes national employment data collected by the
+U.S. Bureau of Labor Statistics in 2018. The report consists of two sections,
+a summary across all industries and a summary across the software publishing
+industry. My supervisor worked with the customer far enough to develop the format of the
+report. It is now my task to analyze a 487MB CSV file to pull out the data
+needed by the report.
+
 ## Expected Behavior
 
 Each subfolder of [data](data) contains a file named `output.txt`.
--- src/main.py
+++ src/main.py
@@ -2,85 +2,107 @@ import time
 import sys
 from Report import Report
 
-
+# create report object
 rpt = Report()
 
+# print usage message and exit if too few arguments are given
 if len(sys.argv) < 2:
     print("Usage: src/main.py DATA_DIRECTORY")
     sys.exit(1)
 
+# start timer and display process message
 print("Reading the databases...", file=sys.stderr)
 before = time.time()
 
+# open area_titles.csv
 areaTitlesPath = sys.argv[1] + "/area_titles.csv"
 currentFile = open(areaTitlesPath)
 areaTitleDict = {}
 
+# start storing desired lines in dictionary
 for line in currentFile:
+    # split line at specific commas (do not remove the state) and then strip the columns of their quotation marks
     columns = line.split(',"')
     columns[0] = columns[0][1:-1]
     columns[1] = columns[1].rstrip()
-    if columns[0].isdigit():
-        doAdd = True
-        a,b,c,d,e = columns[0]
 
+    # determine if the fips code is the one we desire (anything with anything other than digits is not desired)
+    if columns[0].isdigit():
+        doAdd = True  # is the final decision of whether we add it or not
+        a, b, c, d, e = columns[0]  # separates the fips code into its individual digits
+
+        # if the last three digits are all zeros then do not add it
         if c == "0" and d == "0" and e == "0":
             doAdd = False
 
+        # add the desired fips areas to the dictionary
         if doAdd:
             areaTitleDict[columns[0]] = columns[1].rstrip('"')
 
 currentFile.close()
 
+# open 2018.annual.singlefile.csv
 dataPath = sys.argv[1] + "/2018.annual.singlefile.csv"
 currentFile = open(dataPath)
 
+# set default values for the report results
 results = [0, 0, "", 0, 0, "", 0, 0, "", 0, 0, 0, "", 0, 0, "", 0, 0, "", 0]
 
+# iterate through 2019.annual.singlefile.csv and look at the line if it is a fips area in the dictionary
 for line in currentFile:
+    # split the line into columns
     columns = line.split(",")
 
     if areaTitleDict.__contains__(columns[0][1:-1]):
+        # adds the statistics for "all industries"
         if (columns[1].strip('"')).rstrip() == "0" and (columns[2].strip('"')).rstrip() == "10":
-            results[0] += 1
-            results[1] += int(columns[10])
+            results[0] += 1  # increment num of fips areas looked at
+            results[1] += int(columns[10])  # sum of annual total wages
 
+            # determines and sets the area with the maximum for annual wages
             if int(columns[10]) > results[3]:
                 results[2] = areaTitleDict.get(columns[0][1:-1])
                 results[3] = int(columns[10])
 
-            results[4] += int(columns[8])
+            results[4] += int(columns[8])  # sum of establishments in area
 
+            # determines and sets the area with the maximum for establishments
             if int(columns[8]) > results[6]:
                 results[5] = areaTitleDict.get(columns[0][1:-1])
                 results[6] = int(columns[8])
 
-            results[7] += int(columns[9])
+            results[7] += int(columns[9])  # sum of employment in area
 
+            # determines and sets the area with the maximum for employment
             if int(columns[9]) > results[9]:
                 results[8] = areaTitleDict.get(columns[0][1:-1])
                 results[9] = int(columns[9])
 
+        # adds the statistics for "software industries"
         if (columns[1].strip('"')).rstrip() == "5" and (columns[2].strip('"')).rstrip() == "5112":
-            results[10] += 1
-            results[11] += int(columns[10])
+            results[10] += 1  # increment num of fips areas looked at
+            results[11] += int(columns[10])  # sum of annual total wages
 
+            # determines and sets the area with the maximum for annual wages
             if int(columns[10]) > results[13]:
                 results[12] = areaTitleDict.get(columns[0][1:-1])
                 results[13] = int(columns[10])
 
-            results[14] += int(columns[8])
+            results[14] += int(columns[8])  # sum of establishments in area
 
+            # determines and sets the area with the maximum for establishments
             if int(columns[8]) > results[16]:
                 results[15] = areaTitleDict.get(columns[0][1:-1])
                 results[16] = int(columns[8])
 
-            results[17] += int(columns[9])
+            results[17] += int(columns[9])  # sum of employment in area
 
+            # determines and sets the area with the maximum for employment
             if int(columns[9]) > results[19]:
                 results[18] = areaTitleDict.get(columns[0][1:-1])
                 results[19] = int(columns[9])
 
+# end timer and display time it took to gather statistics
 currentFile.close()
 after = time.time()
 print(f"Done in {after - before:.3f} seconds!", file=sys.stderr)
